56ed62b31538cfbe4310f347a061015ddf952142
Roop Basant <72116843+Roopbasant007@users.noreply.github.com>
2023-01-19 20:46:56 +0530

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
+    const DB_URI="mongodb+srv://roop_179:Roop007wa9@cluster0.2dhgovk.mongodb.net/sociogram?retryWrites=true&w=majority"
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.DB_URI);
+    mongoose.connect(DB_URI);
 
     console.log("Database Connected Successfully");
   } catch (error) {
