683607aff745347876747c8eb4750a0002de87ff
Roop Basant <72116843+Roopbasant007@users.noreply.github.com>
2023-01-19 21:14:51 +0530

    Update verifyJWT.js

--- middlewares/verifyJWT.js
+++ middlewares/verifyJWT.js
@@ -9,8 +9,8 @@ const verifyJWT = async (req, res, next) => {
 
     if (!token)
       return res.status(401).json({ message: "Requires Access Token" });
-    const JSON_TOKEN_SECRET = 01b1daf323a9471188a1a4ef298ab750ffacc912b08ff7841955ed10f199dc8f0d705877bdab8bd89ec52577a77a6c136a7c9aae1747930e53ecb3764cb854dc;
-    jwt.verify(token, JSON_TOKEN_SECRET, (error, decoded) => {
+   
+    jwt.verify(token, process.env.JSON_TOKEN_SECRET, (error, decoded) => {
       if (error)
         return res.status(401).json({ message: "User authentication failed" });
       req.id = decoded.id;
